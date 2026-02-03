f83c72ba5d8c8ae790883b60e9d9342260210d27
justGiveMeOneThen <n02219898r@students.nust.ac.zw>
2026-02-03 18:03:24 +0200

    upgrade from sqlLite to PostgreSql

--- mine_safety_project/settings.py
+++ mine_safety_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'mine_safety_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mine_safety_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Fast##2026',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
